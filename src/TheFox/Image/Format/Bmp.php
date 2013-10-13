03e9245e8946b03940bd73b1da4df9c26b719e24
Christian Mayer <thefox21at@gmail.com>
2013-10-13 22:27:47 +0200

    Base converter.

--- src/TheFox/Image/Format/Bmp.php
+++ src/TheFox/Image/Format/Bmp.php
@@ -4,8 +4,73 @@ namespace TheFox\Image\Format;
 
 class Bmp{
 	
+	const BF_FILE_HEADER_SIZE = 14;
+	const BF_INFO_HEADER_SIZE = 40;
+	const BF_BI_RGB = 0;
+	const BF_SIZE_MAX = 0xffffffff;
+	
+	private $bfType = '';
+	private $bfSize = 0;
+	private $biBitCount = 0;
+	private $biCompression = 0;
+	private $biSizeImage = 0;
+	private $biWidth = 0;
+	private $biHeight = 0;
+	
 	public function __construct(){
+		$this->bfType = 'BM';
+		$this->biBitCount = 24;
+		$this->biCompression = static::BF_BI_RGB;
+	}
+	
+	public function setContentSize($size){
+		$this->biSizeImage = $size;
 		
+		$this->bfSize = $this->getFileHeaderSize() + $this->getInfoHeaderSize() + $this->biSizeImage;
+		if($this->bfSize > static::BF_SIZE_MAX){
+			$this->bfSize = static::BF_SIZE_MAX;
+		}
+	}
+	
+	public function setBiWidth($w){
+		$this->biWidth = $w;
+	}
+	
+	public function setBiHeight($h){
+		$this->biHeight = $h;
+	}
+	
+	public function getFileHeaderSize(){
+		return static::BF_FILE_HEADER_SIZE;
+	}
+	
+	public function getFileHeader(){
+		return ''
+			.$this->bfType // bfType
+			.pack('V', $this->bfSize) // bfSize
+			.pack('xxxx') // bfReserved
+			.pack('V', $this->getFileHeaderSize() + $this->getInfoHeaderSize()) // bfOffBits
+			;
+	}
+	
+	public function getInfoHeaderSize(){
+		return static::BF_INFO_HEADER_SIZE;
+	}
+	
+	public function getInfoHeader(){
+		return ''
+			.pack('V', static::BF_INFO_HEADER_SIZE) // biSize
+			.pack('V', $this->biWidth) // biWidth
+			.pack('V', $this->biHeight) // biHeight
+			.pack('v', 1) // biPlanes
+			.pack('v', $this->biBitCount) // biBitCount
+			.pack('V', $this->biCompression) // biCompression
+			.pack('V', 0) // biSizeImage
+			.pack('V', 0) // biXPelsPerMeter
+			.pack('V', 0) // biYPelsPerMeter
+			.pack('V', 0) // biClrUsed
+			.pack('V', 0) // biClrImportant
+			;
 	}
 	
 }
--- upload.php
+++ upload.php
@@ -397,10 +397,82 @@ $logFilesSuccessful->info('end');
 
 
 
+/*
+$c = unpack('c*', 439041101);
+$c = unpack('L', 65 << 24);
+#$c = unpack('L', 'ABCD');
+$c = pack('CCCC', 65, 66, 67, 68 );
+$c = pack('xxxx');
+
+$c = pack('V', 0x424D);
+#$c = pack('V@', 0x4142);
+#$c = pack('@V', 0x4142);
+
+$c = pack('v', 0x4142);
+$c = pack('v@', 0x4142);
+$c = pack('v', 0xaabbcc);
+$c = pack('V', 0xff << 31);
+$c = pack('V', 1);
+$c = pack('v', 65);
+
+
+
+#var_export($c);
+var_dump($c); print "\n\n\n";
+#$c = $c[1];
+
+for($p = 0; $p < strlen($c) - 1; $p++){
+	if(isset($c[$p])){
+		$cc = ord($c[$p]);
+		
+		printf("%02x %02x %02x %02x  %02x %02x %02x %02x \n",
+			($cc >> 56 & 0xff),
+			($cc >> 48 & 0xff),
+			($cc >> 40 & 0xff),
+			($cc >> 32 & 0xff),
+			
+			($cc >> 24 & 0xff),
+			($cc >> 16 & 0xff),
+			($cc >> 8 & 0xff),
+			($cc & 0xff)
+		);
+	}
+	else break;
+}
+
+printf("%02x %02x %02x %02x  %02x %02x %02x %02x \n",
+	($c >> 56 & 0xff),
+	($c >> 48 & 0xff),
+	($c >> 40 & 0xff),
+	($c >> 32 & 0xff),
+	
+	($c >> 24 & 0xff),
+	($c >> 16 & 0xff),
+	($c >> 8 & 0xff),
+	($c & 0xff)
+);
+
+
+print "\n\n\n";
+
+$bitstr = '';
+for($n = 0; $n <= 31; $n++){
+	#print( $n." = ".(int)(   )."\n" );
+	$bitstr = (($c & 1 << $n) > 0 ? '1' : '0'). ($n % 8 == 0 ? ' ' : ''). $bitstr;
+}
+
+print $bitstr."\n";
+*/
+
+
+
 try{
 	$converter = new Converter();
+	$converter->registerExitVar($exit);
+	$converter->convert('test1.txt', 'test1.bmp');
+	#$converter->convert('test2.mkv', 'test2.bmp');
 }
 catch(Exception $e){
-	var_export($e); print "\n";
+	print "ERROR: ".$e->getMessage()."\n";
 }
 
