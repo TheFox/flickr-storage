244dcbb4a96492c01a39ae50ec4200bc0c5a7a7d
Christian Mayer <thefox21at@gmail.com>
2013-10-14 17:59:56 +0200

    Cover & upload.

--- upload.php
+++ upload.php
@@ -30,7 +30,13 @@ $logHandlerFile = new StreamHandler(__DIR__.'/upload.log', Logger::INFO);
 $logHandlerFile->setFormatter($logFormatter);
 $log->pushHandler($logHandlerFile);
 
+$logFilesSuccessfulStream = new StreamHandler(__DIR__.'/upload.files.log', Logger::INFO);
+$logFilesSuccessfulStream->setFormatter($logFormatter);
+$logFilesSuccessful = new Logger('upload');
+$logFilesSuccessful->pushHandler($logFilesSuccessfulStream);
+
 $log->info('start');
+$logFilesSuccessful->info('start');
 
 $exit = 0;
 if(function_exists('pcntl_signal')){
@@ -45,7 +51,6 @@ if(function_exists('pcntl_signal')){
 	pcntl_signal(SIGINT, 'signalHandler');
 }
 
-
 $getopt = new Getopt(array(
     array('d', 'description', Getopt::OPTIONAL_ARGUMENT, 'Description'),
     array('t', 'tags', Getopt::OPTIONAL_ARGUMENT, 'Comma separated tags.'),
@@ -54,7 +59,6 @@ $getopt = new Getopt(array(
 ));
 $getopt->parse();
 
-/*
 if( $getopt->getOption('help') || !count($getopt->getOptions()) ){
 	$getopt->showHelp();
 	exit(3);
@@ -78,16 +82,12 @@ if(
 	exit(1);
 }
 
-
 $photosetNames = array();
 $photosetNamesLower = array();
 if($getopt->getOption('sets')){
 	$photosetNames = preg_split('/,/', $getopt->getOption('sets'));
 	$photosetNamesLower = preg_split('/,/', strtolower($getopt->getOption('sets')));
 }
-
-var_export($photosetNames);
-exit();
 
 $log->info('[main] description: '.($getopt->getOption('description') ? $getopt->getOption('description') : ''));
 $log->info('[main] tags: '.($getopt->getOption('tags') ? $getopt->getOption('tags') : ''));
@@ -174,7 +174,6 @@ try{
 	$xml = $apiFactory->call('flickr.photosets.getList');
 }
 catch(Exception $e){
-	#print "[photoset] list ERROR: ".$e->getMessage()."\n";
 	$log->error('[main] getList: '.$e->getMessage());
 	exit(1);
 }
@@ -206,211 +205,174 @@ foreach($photosetNames as $photosetTitle){
 }
 
 
-if($dh = opendir($argDir)){
-	#print "[dir] upload '".$argDir."'\n";
-	$log->info('[dir] upload dir: '.$argDir);
+foreach($getopt->getOperands() as $filePath){
+	$size = filesize($filePath);
+	$sizeLen = strlen(number_format($size));
 	
-	while( ($file = readdir($dh)) != false ){
-		if($exit) break;
+	$log->info("[file] upload '".$filePath."'  ".siPrefix($size));
+	
+	$pathinfo = pathinfo($filePath);
+	
+	try{
+		$converter = new Converter();
+		$converter->registerExitVar($exit);
 		
-		if(!in_array($file, $FILES_INORE)){
-			$filePath = $argDir.'/'.$file;
-			$size = filesize($filePath);
-			$sizeLen = strlen(number_format($size));
-			$dataLenPrev = 0;
-			$timePrev = time();
-			
-			$fileName = null;
-			$fileExt = '';
+		$outputFilePath = $pathinfo['dirname'].'/'.$pathinfo['basename'].'.bmp';
+		$log->info('[file] cover ...');
+		$converter->cover($filePath, $outputFilePath);
+		
+		
+		if(file_exists($outputFilePath)){
+			$filePath = $outputFilePath;
 			$pathinfo = pathinfo($filePath);
-			#var_export($pathinfo);
-			if(isset($pathinfo['filename'])){
-				$fileName = $pathinfo['filename'];
-			}
-			if(isset($pathinfo['extension'])){
-				$fileExt = strtolower($pathinfo['extension']);
-			}
 			
-			if(in_array($fileExt, $ACCEPTED_EXTENTIONS)){
-				#print "[file] upload '".$file."'  ".siPrefix($size)."\n";
-				$log->info("[file] upload '".$file."'  ".siPrefix($size));
+			$log->info('[file] cover OK');
+		}
+		else{
+			$log->error('[file] cover FAILED');
+			$fileErrors++;
+			$filesFailed[] = $file;
+			continue;
+		}
+	}
+	catch(Exception $e){
+		$log->error('[file] cover FAILED: '.$e->getMessage());
+		$fileErrors++;
+		$filesFailed[] = $file;
+		continue;
+	}
+	
+	
+	$dataLenPrev = 0;
+	$timePrev = time();
+	
+	$fileName = null;
+	$fileExt = '';
+	
+	if(isset($pathinfo['filename'])){
+		$fileName = $pathinfo['filename'];
+	}
+	if(isset($pathinfo['extension'])){
+		$fileExt = strtolower($pathinfo['extension']);
+	}
+	
+	$photoId = 0;
+	$stat = '';
+	$successful = false;
+	
+	$xml = null;
+	try{
+		$xml = $apiFactoryVerbose->upload($filePath, $fileName, $getopt->getOption('description'), $getopt->getOption('tags'));
+		print str_repeat(' ', $termLinePrevLen)."\r";
+	}
+	catch(Exception $e){
+		$log->error('[file] upload: '.$e->getMessage());
+		$xml = null;
+	}
+	
+	if($xml){
+		$photoId = isset($xml->photoid) ? (int)$xml->photoid : 0;
+		$stat = isset($xml->attributes()->stat) ? strtolower((string)$xml->attributes()->stat) : '';
+		$successful = $stat == 'ok' && $photoId != 0;
+	}
+	
+	$logLine = '';
+	if($successful){
+		$logLine = 'OK';
+		$totalFilesUploaded++;
+		
+		$logFilesSuccessful->info(''.$file);
+	}
+	else{
+		$logLine = 'FAILED';
+		$fileErrors++;
+		$filesFailed[] = $file;
+	}
+	$log->info('[file] status: '.$logLine.' - ID '.$photoId);
+	
+	if($successful){
+		
+		if($photosetsNew){
+			foreach($photosetsNew as $photosetTitle){
+				$log->info('[photoset] create '.$photosetTitle.' ... ');
 				
-				$photoId = 0;
-				$stat = '';
-				$successful = false;
+				$photosetId = 0;
+				$xml = null;
+				try{
+					$xml = $apiFactory->call('flickr.photosets.create', array('title' => $photosetTitle, 'primary_photo_id' => $photoId));
+				}
+				catch(Exception $e){
+					$log->critical('[photoset] create '.$photosetTitle.' FAILED: '.$e->getMessage());
+					exit(1);
+				}
+				if($xml){
+					if( (string)$xml->attributes()->stat == 'ok' ){
+						$photosetId = (int)$xml->photoset->attributes()->id;
+						$photosets[] = $photosetId;
+						
+						$log->info('[photoset] create '.$photosetTitle.' OK - ID '.$photosetId);
+					}
+					else{
+						$code = (int)$xml->err->attributes()->code;
+						$log->critical('[photoset] create '.$photosetTitle.' FAILED: '.$code);
+						exit(1);
+					}
+				}
+				else{
+					$log->critical('[photoset] create '.$photosetTitle.' FAILED');
+					exit(1);
+				}
+			}
+			$photosetsNew = null;
+		}
+		
+		if(count($photosets)){
+			$log->info('[file] add to sets ... ');
+			
+			$logLine = '';
+			foreach($photosets as $photosetId){
+				$logLine .= substr($photosetId, -5).' ';
 				
 				$xml = null;
 				try{
-					$xml = $apiFactoryVerbose->upload($filePath, $fileName, $getopt->getOption('description'), $getopt->getOption('tags'));
-					print str_repeat(' ', $termLinePrevLen)."\r";
+					$xml = $apiFactory->call('flickr.photosets.addPhoto', array('photoset_id' => $photosetId, 'photo_id' => $photoId));
 				}
 				catch(Exception $e){
-					#print "\n[file] upload ERROR: ".$e->getMessage()."\n";
-					$log->error('[file] upload: '.$e->getMessage());
-					$xml = null;
+					$log->critical('[file] add to sets FAILED: '.$e->getMessage());
+					exit(1);
 				}
-				
 				if($xml){
-					$photoId = isset($xml->photoid) ? (int)$xml->photoid : 0;
-					$stat = isset($xml->attributes()->stat) ? strtolower((string)$xml->attributes()->stat) : '';
-					$successful = $stat == 'ok' && $photoId != 0;
-				}
-				
-				$logLine = '';
-				if($successful){
-					$logLine = 'OK';
-					$totalFilesUploaded++;
-					
-					$logFilesSuccessful->info(''.$file);
-				}
-				else{
-					$logLine = 'FAILED';
-					$fileErrors++;
-					$filesFailed[] = $file;
-				}
-				$log->info('[file] status: '.$logLine.' - ID '.$photoId);
-				
-				if($successful){
-					if($photosetsNew){
-						foreach($photosetsNew as $photosetTitle){
-							#print '[photoset] create '.$photosetTitle.' ... ';
-							$log->info('[photoset] create '.$photosetTitle.' ... ');
-							
-							$photosetId = 0;
-							$xml = null;
-							try{
-								$xml = $apiFactory->call('flickr.photosets.create', array('title' => $photosetTitle, 'primary_photo_id' => $photoId));
-							}
-							catch(Exception $e){
-								#print "ERROR: ".$e->getMessage()."\n";
-								$log->critical('[photoset] create '.$photosetTitle.' FAILED: '.$e->getMessage());
-								exit(1);
-							}
-							if($xml){
-								if( (string)$xml->attributes()->stat == 'ok' ){
-									$photosetId = (int)$xml->photoset->attributes()->id;
-									$photosets[] = $photosetId;
-									
-									#print 'ID '.$photosetId."\n";
-									$log->info('[photoset] create '.$photosetTitle.' OK - ID '.$photosetId);
-								}
-								else{
-									$code = (int)$xml->err->attributes()->code;
-									#print "FAILED ".$code."\n";
-									$log->critical('[photoset] create '.$photosetTitle.' FAILED: '.$code);
-									exit(1);
-								}
+					if($xml->attributes()->stat == 'ok'){
+						$logLine .= 'OK';
+					}
+					else{
+						if(isset($xml->err)){
+							$code = (int)$xml->err->attributes()->code;
+							if($code == 3){
+								$logLine .= 'OK';
 							}
 							else{
-								#print "FAILED\n";
-								$log->critical('[photoset] create '.$photosetTitle.' FAILED');
+								$log->critical('[file] add to sets FAILED: '.$code);
 								exit(1);
 							}
 						}
-						$photosetsNew = null;
-					}
-					
-					if(count($photosets)){
-						#print "[file] add to sets: ";
-						$log->info('[file] add to sets ... ');
-						
-						$logLine = '';
-						foreach($photosets as $photosetId){
-							#print substr($photosetId, -5).' ';
-							$logLine .= substr($photosetId, -5).' ';
-							
-							$xml = null;
-							try{
-								$xml = $apiFactory->call('flickr.photosets.addPhoto', array('photoset_id' => $photosetId, 'photo_id' => $photoId));
-							}
-							catch(Exception $e){
-								#print "ERROR: ".$e->getMessage()."\n";
-								$log->critical('[file] add to sets FAILED: '.$e->getMessage());
-								exit(1);
-							}
-							if($xml){
-								if($xml->attributes()->stat == 'ok'){
-									#print 'OK';
-									$logLine .= 'OK';
-								}
-								else{
-									if(isset($xml->err)){
-										$code = (int)$xml->err->attributes()->code;
-										if($code == 3){
-											#print 'OK';
-											$logLine .= 'OK';
-										}
-										else{
-											#print 'FAILED '.$code."\n";
-											$log->critical('[file] add to sets FAILED: '.$code);
-											exit(1);
-										}
-									}
-									else{
-										#print 'FAILED'."\n";
-										$log->critical('[file] add to sets FAILED');
-										exit(1);
-									}
-								}
-							}
+						else{
+							$log->critical('[file] add to sets FAILED');
+							exit(1);
 						}
-						#print "\n";
-						
-						$log->info('[file] added to sets: '.$logLine);
 					}
 				}
-				
 			}
-			else{
-				$fileErrors++;
-				$filesFailed[] = $file;
-			}
-			
-			$totalFiles++;
+			$log->info('[file] added to sets: '.$logLine);
 		}
+		
 	}
-	closedir($dh);
-	
 }
 
-#print "\n";
-#print "[main] total uploaded: ".($uploadedTotal > 0 ? siPrefix($uploadedTotal) : 0)."\n";
 $log->info('[main] total uploaded: '.($uploadedTotal > 0 ? siPrefix($uploadedTotal) : 0));
-#print "[main] total files:    ".$totalFiles."\n";
 $log->info('[main] total files:    '.$totalFiles);
-#print "[main] files uploaded: ".$totalFilesUploaded."\n";
 $log->info('[main] files uploaded: '.$totalFilesUploaded);
-#print "[main] files failed:   ".$fileErrors."\n";
 $log->info('[main] files failed:   '.$fileErrors.( count($filesFailed) ? "\n".join("\n", $filesFailed) : '' ));
-if(count($filesFailed)){
-	#print "\n\t ".join("\n\t ", $filesFailed)."\n";
-	#print "\n";
-}
+
 $log->info('end');
 $logFilesSuccessful->info('end');
-*/
-
-
-try{
-	$converter = new Converter();
-	$converter->registerExitVar($exit);
-	
-	#$converter->cover('test2.mkv', 'test2.bmp');
-	#$converter->cover('test3.mov', 'test3.bmp');
-	#$converter->cover('test4.jpg', 'test4.bmp');
-	
-	#foreach($getopt->getOperands() as $file){
-	#	$pathinfo = pathinfo($file);
-	#	$converter->cover($file, $pathinfo['dirname'].'/'.$pathinfo['basename'].'.bmp');
-	#}
-	
-	#$converter->cover('test1.txt', 'test1.bmp');
-	#$converter->recover('test1.bmp', 'test1_2.txt');
-	
-	$converter->cover('test2.mkv', 'test2.bmp');
-	$converter->recover('test2.bmp', 'test2_2.mkv');
-	
-}
-catch(Exception $e){
-	print "ERROR: ".$e->getMessage()."\n";
-}
